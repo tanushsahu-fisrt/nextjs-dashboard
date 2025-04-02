f84efe8ab5318c511991fb1d1d9441badb966bec
tanushsahu-fisrt <sahutanush29@gmail.com>
2025-04-02 22:14:37 +0530

    modified customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -25,7 +25,7 @@ export default async function Page() {
       </div>
 
       {/* Customers Grid */}
-      <div className="bg-white p-6 rounded-lg shadow-md">
+      <div className="bg-white p-6">
         {customers && customers.length > 0 ? (
           <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
             {customers.map((customer) => (
